18b4a40221fdc81939e8f24a223f7a34d9405df6
Smuppers <91326388+Smuppers@users.noreply.github.com>
2024-02-08 11:47:27 -0500

    Update hello.java
    updated hello to csci4485 change

--- hello.java
+++ hello.java
@@ -1,7 +1,7 @@
 public class Hello
 {
  public static void main( String argv[] ){
-    System.out.println("Hello, " + argv[0] + "!");
+    System.out.println("Hello, csci 4485" + argv[0] + "!");
  }
 
 
